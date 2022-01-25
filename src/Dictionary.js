302b1121295d2183d16573ece3a854645b05e2e8
Parnian Naderi <negarintarlan@gmail.com>
2022-01-25 20:34:51 +0330

    Added API URL + other changes
    - Made the design quite responsive (needs work for all the screens)
    - Changed some style.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,43 +1,58 @@
+import { useState } from "react";
+import axios from "axios";
+
 export default function Dictionary() {
+  const [word, setWord] = useState("");
   function handleSubmit(event) {
     event.preventDefault();
-    alert(event.target.search.value);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    axios.get(apiUrl).then((response) => {
+      console.log(response);
+    });
+    //alert(word);
+  }
+  function updateWord(event) {
+    setWord(event.target.value);
   }
   // load a 'word of the day' before search, as homepage!
   return (
     <div className="dict-body mt-5">
       <form onSubmit={handleSubmit}>
         <div className="row d-flex justify-content-center">
-          <div className="col-3">
+          <div className="col-3 d-none d-lg-block">
             <input
               type="text"
               name="search"
               placeholder="Type a word..."
-              className="search-bar d-none d-lg-block"
+              className="search-bar"
+              onChange={updateWord}
             />
           </div>
-          <div className="col-1">
-            <input
-              type="submit"
-              value="Search"
-              className="search-button d-none d-lg-block"
-            />
+          <div className="col-1 d-none d-lg-block">
+            <input type="submit" value="Search" className="search-button" />
           </div>
-          {/* <div className="col-3">
+          <div className="col-5 d-none d-md-block d-lg-none">
             <input
               type="text"
               name="search"
               placeholder="Type a word..."
-              className="search-bar-sm d-lg-none d-md-block"
+              className="search-bar-md"
             />
           </div>
-          <div className="col-1">
+          <div className="col-2 d-none d-md-block d-lg-none">
+            <input type="submit" value="Search" className="search-button-md" />
+          </div>
+          <div className="col-7 d-md-none">
             <input
-              type="submit"
-              value="Search"
-              className="search-button-sm d-lg-none d-md-block"
+              type="text"
+              name="search"
+              placeholder="Type a word..."
+              className="search-bar-sm"
             />
-          </div> */}
+          </div>
+          <div className="col-3 d-md-none">
+            <input type="submit" value="Search" className="search-button-sm" />
+          </div>
         </div>
       </form>
     </div>
