505bf0121007e9ee434493062f1459d958e96de7
Parnian Naderi <negarintarlan@gmail.com>
2022-01-26 21:57:07 +0330

    Added support for word origin
    + added another icon
    + better style for synonyms and antonyms
    + added developer info (yet to be added to the main page)

--- src/Meaning.js
+++ src/Meaning.js
@@ -7,18 +7,18 @@ export default function Meaning(props) {
 
   return (
     <div>
-      <div className="meaning p-3">
+      <div className="meaning p-4">
         <div className="part-of-speech">{props.def.partOfSpeech}</div>
         {props.def.definitions.map(function (definition, index) {
           return (
             <div className="ms-4 my-4" key={index}>
               {/* <strong>Definition: </strong> */}
-              <div className="definition mb-2">- {definition.definition}</div>
+              <li className="definition mb-2">{definition.definition}</li>
               {/* <strong>Example: </strong> */}
-              <div className="example mb-2">
+              <div className="example mb-4">
                 <em>{definition.example}</em>
               </div>
-              <div className="ms-3">
+              <div className="mb-3">
                 <Synonyms data={definition.synonyms} />
                 <Antonyms data={definition.antonyms} />
               </div>
--- src/Results.js
+++ src/Results.js
@@ -1,25 +1,34 @@
 import Meaning from "./Meaning";
+import Origin from "./Origin";
 
 export default function Results(props) {
   const meanings = props.data[0]["meanings"];
-  //const phonetics = props.data[0]["phonetics"]
-  //const origin = props.data[0]["origin"]
+  const phonetics = props.data[0]["phonetics"];
+  const origin = props.data[0]["origin"];
 
   //order: phonetics definition origin
   return (
     <div className="container-fluid">
       <div className="results">
         <div className="row">
-          <div className="col-9 mx-auto text-left">
+          <div className="col-10 mx-auto text-left"></div>
+        </div>
+        <div className="row">
+          <div className="col-10 mx-auto text-left">
             <div className="meanings d-flex flex-column">
               {meanings.map((meaning, index) => {
                 return (
-                  <div className="py-1" key={index}>
+                  <div className="py-2" key={index}>
                     <Meaning def={meaning} key={index} />
                   </div>
                 );
               })}
             </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-10 mx-auto text-left py-2">
+            <Origin data={origin} />
           </div>
         </div>
       </div>
